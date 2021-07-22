39bf6d7ffd2c90d3afd9c72842f9720726cca175
Keyvan Mosharraf <github@keyvanm.com>
2021-07-22 15:12:38 -0700

    HasUUIDExample

--- example/sample/admin.py
+++ example/sample/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+
+from building_blocks.admin.blocks import HasUUIDAdminBlock
+from sample.models import HasUUIDExample
+
+
+@admin.register(HasUUIDExample)
+class HasUUIDExampleAdmin(admin.ModelAdmin):
+    search_fields = (
+        *HasUUIDAdminBlock.search_fields,
+    )
+    list_display = (
+        *HasUUIDAdminBlock.list_display,
+    )
+
+    readonly_fields = (
+        *HasUUIDAdminBlock.readonly_fields,
+    )
+    fieldsets = (
+        *HasUUIDAdminBlock.fieldsets,
+    )
--- example/sample/models.py
+++ example/sample/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+from building_blocks.abstracts import HasUUID
+
+
+class HasUUIDExample(
+    HasUUID,
+    models.Model
+):
+    pass
