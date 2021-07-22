3ccf267f64dcfef011cec0c7ee777d4b582166ac
Keyvan Mosharraf <github@keyvanm.com>
2021-07-22 15:41:16 -0700

    Publishable abstract model

--- building_blocks/abstracts.py
+++ building_blocks/abstracts.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.utils.timezone import now
 
+from .enums import PublishingStage
 from .querysets import ArchivableQueryset
 
 
@@ -42,3 +43,34 @@ class Archivable(models.Model):
         self.archived_at = None
 
     objects = ArchivableQueryset.as_manager()
+
+
+class Publishable(models.Model):
+    class Meta:
+        abstract = True
+
+    publishing_stage = models.CharField(max_length=30, choices=PublishingStage.choices, default=PublishingStage.draft)
+    publishing_stage_changed_at = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def is_active(self):
+        return self.publishing_stage == PublishingStage.published
+
+    def publish(self):
+        assert self.publishing_stage == PublishingStage.draft, "Can only publish items in draft"
+        self.publishing_stage = PublishingStage.published
+        self.publishing_stage_changed_at = now()
+
+    def unpublish(self):
+        assert self.publishing_stage == PublishingStage.published, "Can only unpublish items that are already published"
+        self.publishing_stage = PublishingStage.draft
+        self.publishing_stage_changed_at = now()
+
+    def archive(self):
+        self.publishing_stage = PublishingStage.archived
+        self.publishing_stage_changed_at = now()
+
+    def restore(self):
+        assert self.publishing_stage == PublishingStage.archived, "Can only restore items in archive"
+        self.publishing_stage = PublishingStage.draft
+        self.publishing_stage_changed_at = now()
