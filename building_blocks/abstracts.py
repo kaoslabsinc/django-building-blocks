b30c0343c088a0ae3364ca00218ef26cf04d8ee6
Keyvan Mosharraf <github@keyvanm.com>
2021-07-22 15:23:40 -0700

    Archivable abstract model

--- building_blocks/abstracts.py
+++ building_blocks/abstracts.py
@@ -1,6 +1,9 @@
 import uuid
 
 from django.db import models
+from django.utils.timezone import now
+
+from .querysets import ArchivableQueryset
 
 
 class HasUUID(models.Model):
@@ -16,3 +19,26 @@ class HasUUID(models.Model):
     @property
     def shortcode(self):
         return self.uuid_str.split('-')[0]
+
+
+class Archivable(models.Model):
+    class Meta:
+        abstract = True
+
+    archived_at = models.DateTimeField(null=True, blank=True)
+
+    @property
+    def is_active(self):
+        return self.archived_at is None
+
+    @property
+    def archive_status(self):
+        return 'archived' if self.archived_at else 'active'
+
+    def archive(self):
+        self.archived_at = now()
+
+    def restore(self):
+        self.archived_at = None
+
+    objects = ArchivableQueryset.as_manager()
