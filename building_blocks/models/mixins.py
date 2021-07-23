6916e10e01b05a9138a8d3d218cf114672396e8e
Keyvan Mosharraf <github@keyvanm.com>
2021-07-22 17:04:32 -0700

    Docstring for HasInitials

--- building_blocks/models/mixins.py
+++ building_blocks/models/mixins.py
@@ -2,6 +2,11 @@ from building_blocks.utils import create_initials
 
 
 class HasInitials:
+    """
+    Add property `initials` to its inheritor classes. Take the value form the field defined by `take_initials_from` and
+    return the initial letters of each word in it, capitalized.
+    Example: John Smith => JS
+    """
     take_initials_from = None
 
     @property
