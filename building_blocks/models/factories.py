cb8020ffd9d71897bb8172e0c64e69756b6cb490
Keyvan Mosharraf <github@keyvanm.com>
2021-07-26 15:52:04 -0700

    HasNameFactory

--- building_blocks/models/factories.py
+++ building_blocks/models/factories.py
@@ -1,4 +1,22 @@
+from django.db import models
+
+
 class AbstractModelFactory:
     @staticmethod
     def as_abstract_model(**kwargs):
         raise NotImplementedError
+
+
+class HasNameFactory(AbstractModelFactory):
+    @staticmethod
+    def as_abstract_model(optional=False):
+        class HasName(models.Model):
+            class Meta:
+                abstract = True
+
+            name = models.CharField(max_length=255, blank=optional)
+
+            def __str__(self):
+                return self.name if self.name else super(HasName, self).__str__()
+
+        return HasName
