3e85324e082c876e8c318eed49f54894e4d71a98
Keyvan Mosharraf <github@keyvanm.com>
2021-07-30 10:00:08 -0700

    Render html elements

--- building_blocks/admin/utils.py
+++ building_blocks/admin/utils.py
@@ -1,5 +1,6 @@
 import json
 
+from django.utils.html import format_html_join, format_html
 from django.utils.safestring import mark_safe
 from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
@@ -12,3 +13,40 @@ def json_field_pp(d):
     response = highlight(response, JsonLexer(), formatter)
     style = "<style>" + formatter.get_style_defs() + "</style><br>"
     return mark_safe(style + response)
+
+
+def render_attrs(attrs):
+    return format_html_join(
+        ' ', '{}="{}"', ((k, v) for k, v in attrs.items())
+    )
+
+
+def render_element(tag, children=None, attrs=None):
+    attrs = attrs or None
+    if children is None:
+        return format_html(
+            """<{} {}>""",
+            tag, render_attrs(attrs)
+        )
+    else:
+        return format_html(
+            """<{tag} {attrs}>{children}</{tag}>""",
+            tag=tag, attrs=render_attrs(attrs), children=children
+        )
+
+
+def render_img(src, alt="", attrs=None):
+    attrs = attrs or {}
+    attrs['src'] = src
+    if alt:
+        attrs['alt'] = alt
+    return render_element('img', attrs=attrs)
+
+
+def render_anchor(href, children, new_tab=True, attrs=None):
+    attrs = attrs or {}
+    attrs['href'] = href
+    if new_tab:
+        attrs['target'] = '_blank'
+        attrs['rel'] = 'noreferrer'
+    return render_element('a', children, attrs=attrs)
