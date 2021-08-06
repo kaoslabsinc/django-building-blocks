1dbbfe31149735b4a928632d05ad7c9aee3f7f9a
Keyvan Mosharraf <github@keyvanm.com>
2021-08-05 18:17:15 -0700

    Hide change actions from users who don't have change permissions in PublishableAdmin

--- building_blocks/admin/admin.py
+++ building_blocks/admin/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_permission_codename
 from django.db.models import Q
 from django.utils.timezone import now
-from django_object_actions import DjangoObjectActions, takes_instance_or_queryset
+from django_object_actions import takes_instance_or_queryset
 
 from .mixins import CheckUserAdminMixin, DjangoObjectActionsPermissionsMixin
 from ..models import Archivable, Publishable
@@ -39,7 +39,7 @@ class ArchivableAdmin(DjangoObjectActionsPermissionsMixin, admin.ModelAdmin):
         return change_actions
 
 
-class PublishableAdmin(DjangoObjectActions, admin.ModelAdmin):
+class PublishableAdmin(DjangoObjectActionsPermissionsMixin, admin.ModelAdmin):
     change_actions = ('publish', 'unpublish', 'archive', 'restore')
 
     @admin.action(permissions=['change'])
@@ -64,17 +64,17 @@ class PublishableAdmin(DjangoObjectActions, admin.ModelAdmin):
 
     def get_change_actions(self, request, object_id, form_url):
         change_actions = super().get_change_actions(request, object_id, form_url)
-        change_actions = list(change_actions)
-        obj: Publishable = self.model.objects.get(pk=object_id)
-
-        if obj.publishing_stage != PublishingStage.archived:
-            change_actions.remove('restore')
-        if obj.publishing_stage != PublishingStage.published:
-            change_actions.remove('unpublish')
-        if obj.publishing_stage != PublishingStage.draft:
-            change_actions.remove('publish')
-        if obj.publishing_stage == PublishingStage.archived:
-            change_actions.remove('archive')
+        if change_actions:
+            change_actions = list(change_actions)
+            obj: Publishable = self.__obj
+            if obj.publishing_stage != PublishingStage.archived:
+                change_actions.remove('restore')
+            if obj.publishing_stage != PublishingStage.published:
+                change_actions.remove('unpublish')
+            if obj.publishing_stage != PublishingStage.draft:
+                change_actions.remove('publish')
+            if obj.publishing_stage == PublishingStage.archived:
+                change_actions.remove('archive')
 
         return change_actions
 
