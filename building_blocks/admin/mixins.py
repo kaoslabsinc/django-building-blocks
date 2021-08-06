e9043c62fc313fe69069dc5c1d867452a9c90fa3
Keyvan Mosharraf <github@keyvanm.com>
2021-08-06 14:52:38 -0700

    Remove has_xxx_permission methods as by default filtering objects out of the queryset also limits access to them. Resolves #4

--- building_blocks/admin/mixins.py
+++ building_blocks/admin/mixins.py
@@ -4,32 +4,15 @@ from django_object_actions import DjangoObjectActions
 
 class CheckUserAdminMixin(BaseModelAdmin):
     """
-    Limit access to objects who don't pass the check denoted by check_user() or has_see_all_permission().
+    Limit access to objects who don't pass the check denoted by has_see_all_permission() or the queryset filtered by
+    check_user_q()
     """
 
     def has_see_all_permission(self, request):
         raise NotImplementedError
 
-    def check_user(self, request, obj):
-        raise NotImplementedError
-
     def check_user_q(self, request):
         raise NotImplementedError
-
-    def has_view_permission(self, request, obj=None):
-        return super(CheckUserAdminMixin, self).has_view_permission(request, obj) and (
-            obj is None or self.has_see_all_permission(request) or self.check_user(request, obj)
-        )
-
-    def has_change_permission(self, request, obj=None):
-        return super(CheckUserAdminMixin, self).has_change_permission(request, obj) and (
-            obj is None or self.has_see_all_permission(request) or self.check_user(request, obj)
-        )
-
-    def has_delete_permission(self, request, obj=None):
-        return super(CheckUserAdminMixin, self).has_delete_permission(request, obj) and (
-            obj is None or self.has_see_all_permission(request) or self.check_user(request, obj)
-        )
 
     def get_queryset(self, request):
         qs = super(CheckUserAdminMixin, self).get_queryset(request)
