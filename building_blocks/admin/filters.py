ee866bcedeffd3b4315070825fb20a559a4dfbee
Keyvan Mosharraf <github@keyvanm.com>
2022-01-24 19:24:15 -0800

    Allow setting verbose names for filters in QuerysetChoiceFilter

--- building_blocks/admin/filters.py
+++ building_blocks/admin/filters.py
@@ -6,7 +6,14 @@ class QuerysetChoiceFilter(admin.SimpleListFilter):
     queryset_filters = ()
 
     def lookups(self, request, model_admin):
-        return [(filter_name, filter_name.replace('_', ' ').capitalize()) for filter_name in self.queryset_filters]
+        lookups = []
+        for filter_def in self.queryset_filters:
+            if isinstance(filter_def, tuple):
+                key, verbose_name = filter_def
+            else:
+                key, verbose_name = filter_def, filter_def.replace('_', ' ').capitalize()
+            lookups.append((key, verbose_name))
+        return lookups
 
     def queryset(self, request, queryset):
         value = self.value() or self.default_value
