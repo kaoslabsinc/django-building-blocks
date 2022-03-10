a8ac82fd53d8c2e2e1ba5a81d37909f8961fc4b4
Keyvan Mosharraf <github@keyvanm.com>
2022-03-10 15:40:48 -0800

    Viewsets

---
+++ building_blocks/rest/viewsets.py
@@ -0,0 +1,17 @@
+from django.db import models
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import GenericAPIView
+
+
+class QSFiltersViewSetMixin(GenericAPIView):
+
+    def get_queryset(self):
+        qs: models.QuerySet = super(QSFiltersViewSetMixin, self).get_queryset()
+
+        if qs_method := self.request.query_params.get('qs'):
+            try:
+                qs = getattr(qs, qs_method)(user=self.request.user)
+            except AttributeError:
+                raise ValidationError({'qs': f"{qs_method} is not a valid choice"})
+
+        return qs
