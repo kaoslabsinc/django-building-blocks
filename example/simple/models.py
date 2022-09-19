791a76d0c8263c850c6ee984d189c3215b3108a2
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-19 18:14:33 -0400

    test_SluggedKaosModel

--- example/simple/models.py
+++ example/simple/models.py
@@ -1,1 +1,7 @@
-from django.db import models
+from dj_kaos_utils.models import MoneyField
+
+from building_blocks.models import SluggedKaosModel, Archivable
+
+
+class Product(Archivable, SluggedKaosModel):
+    price = MoneyField()
