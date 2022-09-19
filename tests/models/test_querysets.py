c1a822a16fa6b436e581d95978da91c1495877b2
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-19 18:18:09 -0400

    test_ArchivableQuerySet

--- tests/models/test_querysets.py
+++ tests/models/test_querysets.py
@@ -1,2 +1,32 @@
-def test_ArchivableQuerySet():
-    pass
+import pytest
+
+from simple.models import Product
+
+
+def test_ArchivableQuerySet(db):
+    product = Product.objects.create(name="Name", price=10)
+    assert Product.objects.available().count() == 1
+    assert Product.objects.archived().count() == 0
+
+    product.archive()
+    product.save()
+    assert Product.objects.available().count() == 0
+    assert Product.objects.archived().count() == 1
+
+    product.refresh_from_db()
+    product.restore()
+    product.save()
+    assert Product.objects.available().count() == 1
+    assert Product.objects.archived().count() == 0
+
+
+def test_ArchivableQuerySet__raises(db):
+    product = Product.objects.create(name="Name", price=10)
+
+    with pytest.raises(AssertionError):
+        product.restore()
+
+    product.archive()
+    product.save()
+    with pytest.raises(AssertionError):
+        product.archive()
