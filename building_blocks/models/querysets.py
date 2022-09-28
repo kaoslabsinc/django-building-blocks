42e80e181f16a49a05761082da0412da67dfeca0
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-28 08:04:01 -0400

    docs

--- building_blocks/models/querysets.py
+++ building_blocks/models/querysets.py
@@ -2,16 +2,35 @@ from django.db import models
 
 
 class ArchivableQuerySet(models.QuerySet):
+    """
+    Default queryset interface for Archivable objects. Adds queryset methods to interact with the Archivable interface.
+    """
     def available(self):
+        """
+        :return: queryset with db rows that are "available" (aka not archived)
+        """
         return self.filter(is_archived=False)
 
     def archived(self):
+        """
+        :return: queryset with db rows that are archived
+        """
         return self.filter(is_archived=True)
 
     def set_archived(self):
+        """
+        Archive the objects in this queryset.
+
+        :return: the return value from `.update()` i.e. the count of rows updated.
+        """
         return self.update(is_archived=True)
 
     def set_restored(self):
+        """
+        Restore (unarchive) the objects in this queryset.
+
+        :return: the return value from `.update()` i.e. the count of rows updated.
+        """
         return self.update(is_archived=False)
 
 
