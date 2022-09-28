5ecc68f0795892a8e785c7edbfdec4c450c86c73
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-09-27 17:03:16 -0700

    Admin mixins: django object actions

--- building_blocks/admin/mixins.py
+++ building_blocks/admin/mixins.py
@@ -1,4 +1,5 @@
 from django.contrib.admin.options import BaseModelAdmin
+from django_object_actions import BaseDjangoObjectActions
 
 
 class PrepopulateSlugAdminMixin(BaseModelAdmin):
@@ -16,3 +17,42 @@ class PrepopulateSlugAdminMixin(BaseModelAdmin):
         if obj:  # editing an existing object
             return prepopulated_fields
         return {**prepopulated_fields, self.slug_field: (self.slug_source,)}
+
+
+class DjangoObjectActionsPermissionsMixin(BaseDjangoObjectActions):
+    """
+    Built on DjangoObjectActions Admin, it checks if the user has change permissions on the object in order to show the
+    change actions
+    """
+
+    def get_change_actions(self, request, object_id, form_url):
+        obj = self.model.objects.get(pk=object_id)
+        self.__obj = obj
+        if not self.has_change_permission(request, obj):
+            return ()
+        else:
+            return super(DjangoObjectActionsPermissionsMixin, self).get_change_actions(request, object_id, form_url)
+
+    def _get_change_action_object(self):
+        return self.__obj
+
+
+class AreYouSureActionsAdminMixin(BaseDjangoObjectActions):
+    are_you_sure_actions = ()
+    are_you_sure_prompt_f = "Are you sure you want to {label} this object?"
+
+    def __init__(self, *args, **kwargs):
+        super(AreYouSureActionsAdminMixin, self).__init__(*args, **kwargs)
+        for action in self.are_you_sure_actions:
+            tool = getattr(self, action)
+            label = getattr(tool, 'label', action).lower()
+            are_you_sure_prompt = self.are_you_sure_prompt_f.format(tool=tool, label=label)
+            tool.__dict__.setdefault('attrs', {})
+            tool.__dict__['attrs'].setdefault('onclick', f"""return confirm("{are_you_sure_prompt}");""")
+
+
+__all__ = [
+    'PrepopulateSlugAdminMixin',
+    'DjangoObjectActionsPermissionsMixin',
+    'AreYouSureActionsAdminMixin',
+]
