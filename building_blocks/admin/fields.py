89a098c7f7576c4843409f19e0221aa56b21d359
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-10-26 17:17:56 -0400

    fields.py with name of common fields for admin consumption

---
+++ building_blocks/admin/fields.py
@@ -0,0 +1,14 @@
+UUID = 'uuid'
+SLUG = 'slug'
+
+CREATED = 'created'
+MODIFIED = 'modified'
+
+NAME = 'name'
+DESCRIPTION = 'description'
+IMAGE = 'image'
+
+CATEGORY = 'category'
+TYPE = 'type'
+
+NOTES = 'notes'
