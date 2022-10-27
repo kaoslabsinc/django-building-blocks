68fcae0295d2f6e170676be976186134c6c3c636
Keyvan Mosharraf <keyvan@keyvanm.com>
2022-10-27 03:34:42 -0400

    also enable unrequire slug

--- building_blocks/models/admin/mixins.py
+++ building_blocks/models/admin/mixins.py
@@ -1,4 +1,5 @@
 from dj_kaos_utils.admin import EditReadonlyAdminMixin
+from dj_kaos_utils.forms import unrequire_form
 
 from building_blocks.admin import PrepopulateSlugAdminMixin
 from building_blocks.consts.field_names import *
@@ -13,6 +14,10 @@ class EnhancedSluggedKaosModelAdminMixin(
     EnhancedHasSlugModelAdminMixin
 ):
     slug_source = NAME
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        return unrequire_form(form, ('slug',)) if not obj else form
 
 
 __all__ = (
