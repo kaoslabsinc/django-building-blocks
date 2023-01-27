9381d2609cadb9aab78361d00c83915ca75e593c
Keyvan Mosharraf <keyvan@keyvanm.com>
2023-01-27 14:49:29 -0500

    Docstrings

--- building_blocks/admin/base.py
+++ building_blocks/admin/base.py
@@ -5,6 +5,16 @@ from .mixins import EnhancedSluggedKaosModelAdminMixin
 
 
 class BaseKaosModelAdmin(BaseModelAdmin):
+    """
+    Base model admin for `KaosModel`
+
+    :param ordering: None
+    :param exclude: None
+    :param fields: None
+    :param autocomplete_fields: ()
+    :param readonly_fields: ('uuid', 'created', 'modified')
+    :param fieldsets: ((None, {'fields': (('name',),)}), ('Admin', {'fields': ('uuid', 'created')}))
+    """
     readonly_fields = KaosModelAdminBlock.readonly_fields
     fieldsets = (
         (None, {'fields': (KaosModelAdminBlock.base_fields,)}),
@@ -13,6 +23,11 @@ class BaseKaosModelAdmin(BaseModelAdmin):
 
 
 class BaseKaosModelAdminExtra(BaseKaosModelAdmin):
+    """
+    `BaseKaosModelAdmin` with extra admin fields (modified)
+
+    :param fieldsets: ((None, {'fields': (('name',),)}), ('Admin', {'fields': ('uuid', 'created', 'modified')}))
+    """
     fieldsets = (
         *BaseKaosModelAdmin.fieldsets[:-1],
         KaosModelAdminBlock.the_admin_fieldset_extra,
--- docs/admin.rst
+++ docs/admin.rst
@@ -1,7 +1,17 @@
 Admin
-=====
+*****
+
+.. contents::
+    :depth: 2
 
 .. toctree::
    :maxdepth: 2
 
    admin/blocks
+
+Base
+=====
+.. autoclass:: building_blocks.admin.base.BaseKaosModelAdmin
+
+.. autoclass:: building_blocks.admin.base.BaseKaosModelAdminExtra
+    :show-inheritance:
